f3a58d2db0eb621f7b4570a6604e69c0e5155bb0
Tom Lamphier <thomaslamphier@gmail.com>
2021-05-02 21:46:02 -0400

    upgrade Leaflet to 2+

--- src/main.js
+++ src/main.js
@@ -18,14 +18,14 @@ Vue.use(VueAxios, axios)
 Vue.use(DatatableFactory)
 
 Vue.config.productionTip = false
-Vue.prototype.$serverurl = 'http://localhost:8080'
-delete L.Icon.Default.prototype._getIconUrl
+Vue.prototype.$serverurl = 'http://datasciex.com:8080'
+//delete L.Icon.Default.prototype._getIconUrl
 
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-})
+//L.Icon.Default.mergeOptions({
+//  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+//  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+//  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+//})
 
 
 /* eslint-disable no-new */
